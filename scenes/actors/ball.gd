4f20fbd63a6625b6e0159e8a4bd76a6077d63ae1
Sparkworm <imnota.squirp@gmail.com>
2025-03-03 15:39:12 -0600

    Split bouce into two functions.

--- scenes/actors/ball.gd
+++ scenes/actors/ball.gd
@@ -12,12 +12,14 @@ func _physics_process(_delta: float) -> void:
 	var collision := move_and_collide(velocity)
 	if collision:
 		bounce(collision)
+		hit_tile(collision)
 
 ## Changes direction after collision
 func bounce(col: KinematicCollision2D) -> void:
 	velocity = velocity.bounce(col.get_normal())
+
+func hit_tile(col: KinematicCollision2D) -> void:
 	if col.get_collider() is Tiles:
-		print("tilemap hit")
 		var tilemap = col.get_collider() as Tiles
-		var coords:Vector2i = tilemap.get_coords_for_body_rid(col.get_collider_rid())
+		var coords: Vector2i = tilemap.get_coords_for_body_rid(col.get_collider_rid())
 		tilemap.demote_tile(coords)
