a6a1ec332cab18490ed204f9e6cb3a007c4be976
Sparkworm <imnota.squirp@gmail.com>
2025-03-10 15:09:55 -0500

    Added ability to remove barriers

--- scenes/parent/level.gd
+++ scenes/parent/level.gd
@@ -5,6 +5,7 @@ extends GameScene
 @export var barrier: PackedScene
 
 @onready var player_barriers: Node = $PlayerBarriers
+@onready var barrier_detector: RayCast2D = $BarrierDetector
 
 var last_barrier: PlayerBarrier
 
@@ -16,6 +17,8 @@ func _process(delta:float) -> void:
 	if Input.is_action_just_released("place_barrier"):
 		last_barrier.enable()
 		last_barrier = null
+	if Input.is_action_just_pressed("destroy_barrier"):
+		destroy_barrier()
 
 func place_barrier(coords:Vector2) -> void:
 	var new_bar = barrier.instantiate() as PlayerBarrier
@@ -23,6 +26,16 @@ func place_barrier(coords:Vector2) -> void:
 	player_barriers.add_child(new_bar)
 	new_bar.disable()
 	last_barrier = new_bar
+
+func destroy_barrier() -> void:
+	barrier_detector.position = get_local_mouse_position()
+	barrier_detector.target_position = Vector2(0,0)
+	barrier_detector.force_raycast_update()
+	var barrier_col = barrier_detector.get_collider()
+	print(barrier_col)
+	barrier_col = barrier_col as PlayerBarrier
+	if barrier_col:
+		barrier_col.queue_free()
 
 func update_barrier_angle() -> void:
 	if last_barrier:
